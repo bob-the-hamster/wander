84cdba8c2f439d531daadd4cb300cb46d2d2504d
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2008-10-29 16:47:47 +0000

    Wander: probably best to bundle the exe in the zip file since Wander's text boxes actually require the wip autoshrink feature
    git-svn-id: https://rpg.hamsterrepublic.com/source/games/wander@2404 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.bat
+++ distrib.bat
@@ -14,7 +14,7 @@ cp %USEVER%\game.exe wander.exe
 rm Output\wandering-hamster.exe
 "C:\Program Files\Inno Setup 5\iscc" wander.iss
 rm Output\wandering-hamster.zip
-zip Output\wandering-hamster.zip wander.rpg LICENSE-*.txt readme-wander.txt readme-how-do-I-play.txt wanderp.hss
+zip Output\wandering-hamster.zip wander.exe wander.rpg LICENSE-*.txt readme-wander.txt wanderp.hss
 
 rm wander.rpg wander.exe SDL*.dll
 pause
--- readme-how-do-I-play.txt
+++
@@ -1,8 +0,0 @@
-You downloaded the data-file-only version of Wandering Hamster.
-This means that you need to install the OHRRPGCE before you can play it.
-
-=WINDOWS=
-Read http://HamsterRepublic.com/ohrrpgce/index.php/Installation.html
-
-=LINUX=
-Read http://HamsterRepublic.com/ohrrpgce/index.php/Linux_Notes.html
