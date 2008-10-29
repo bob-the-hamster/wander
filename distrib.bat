22945fdbc7e4fe8fc667f2dd2bc305b6f28a4d8d
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2008-10-29 15:42:33 +0000

    Wander: another distrib script fix
    git-svn-id: https://rpg.hamsterrepublic.com/source/games/wander@2402 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.bat
+++ distrib.bat
@@ -1,3 +1,4 @@
+IF EXIST ..\..\wip%1 GOTO usewip
 IF NOT EXIST ..\..\rel\%1 GOTO usewip
 set USEVER=..\..\rel\%1
 GOTO begin
@@ -10,8 +11,9 @@ cp %USEVER%\SDL.dll .
 cp %USEVER%\SDL_mixer.dll .
 cp %USEVER%\game.exe wander.exe
 ..\..\wip\relump wander.rpgdir
+rm Output\wandering-hamster.exe
 "C:\Program Files\Inno Setup 5\iscc" wander.iss
-
+rm Output\wandering-hamster.zip
 zip Output\wandering-hamster.zip wander.rpg LICENSE-*.txt readme-wander.txt readme-how-do-I-play.txt wanderp.hss
 
 rm wander.rpg wander.exe SDL*.dll
