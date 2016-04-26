1639cefd4b6ee80eb657b744892d82f848d417d2
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2016-04-26 04:16:00 +0000

    Wander: more Gallopeg trees, fix Rathmara's Fireblitz to always spread
    git-svn-id: https://rpg.hamsterrepublic.com/source/games/wander@7960 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.sh
+++ distrib.sh
@@ -1,76 +1,17 @@
 #!/bin/sh
 
-RELUMP=~/src/ohr/wip/relump
-
-rm -Rf distrib
-mkdir distrib
-cd distrib
-
-wget http://hamsterrepublic.com/ohrrpgce/nightly/ohrrpgce-wip-default.zip
-unzip ohrrpgce-wip-default.zip game.exe *.dll svninfo.txt
-rm ohrrpgce-wip-default.zip
-
-SVN=`grep "^Last Changed Rev:" svninfo.txt | cut -d " " -f 4`
-DATE=`grep "^Last Changed Date:" svninfo.txt | cut -d " " -f 4 | tr "-" "."`
-
-mv game.exe wander.exe
-"${RELUMP}" ../wander.rpgdir ./wander.rpg
-cp ../LICENSE.txt .
-cp ../README.txt .
-cp ../wander.hss .
-cp ../wander.ico .
-
-# Create the zip file
-rm ../wandering-hamster.zip
-zip ../wandering-hamster.zip wander.exe wander.rpg LICENSE.txt README.txt *.dll wander.hss
-
-# Create the Windows installer
-cat << EOF > wander.iss
-[Setup]
-AppName=Wandering Hamster
-AppVerName=Wandering Hamster ${DATE}.${SVN}
-VersionInfoVersion=${DATE}.${SVN}
-AppPublisher=Hamster Republic Productions
-AppPublisherURL=http://HamsterRepublic.com/ohrrpgce/
-AppSupportURL=http://HamsterRepublic.com/ohrrpgce/index.php/Game_Wandering_Hamster.html
-AppUpdatesURL=http://HamsterRepublic.com/ohrrpgce/index.php/Game_Wandering_Hamster.html
-AppReadmeFile={app}\README.txt
-DefaultDirName={pf}\Hamster Republic\Wandering Hamster
-DefaultGroupName=Wandering Hamster
-DisableProgramGroupPage=yes
-AllowNoIcons=yes
-AllowUNCPath=no
-LicenseFile=LICENSE.txt
-InfoAfterFile=README.txt
-OutputBaseFilename=wandering-hamster
-Compression=bzip
-SolidCompression=yes
-ChangesAssociations=no
-UninstallDisplayIcon={app}\wander.ico
-
-[Languages]
-Name: "eng"; MessagesFile: "compiler:Default.isl"
-
-[Tasks]
-Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"
-
-[Files]
-Source: "wander.exe"; DestDir: "{app}"; Flags: ignoreversion
-Source: "SDL.dll"; DestDir: "{app}"; Flags: ignoreversion
-Source: "SDL_mixer.dll"; DestDir: "{app}"; Flags: ignoreversion
-Source: "wander.rpg"; DestDir: "{app}"; Flags: ignoreversion
-Source: "wander.hss"; DestDir: "{app}"; Flags: ignoreversion
-Source: "wander.ico"; DestDir: "{app}"; Flags: ignoreversion
-Source: "README.txt"; DestDir: "{app}"; Flags: ignoreversion
-Source: "LICENSE.txt"; DestDir: "{app}"; Flags: ignoreversion
-; NOTE: Don't use "Flags: ignoreversion" on any shared system files
-
-[Icons]
-Name: "{group}\Wandering Hamster"; Filename: "{app}\wander.exe"; Flags: closeonexit
-Name: "{group}\Wandering Hamster (fullscreen)"; Filename: "{app}\wander.exe"; Parameters: "-f"; Flags: closeonexit
-Name: "{userdesktop}\Wandering Hamster"; Filename: "{app}\wander.exe"; Flags: closeonexit; Tasks: desktopicon
-EOF
-
-wine "C:\Program Files\Inno Setup 5\iscc" wander.iss
-mv Output/wandering-hamster.exe ../
-rmdir Output
+cd ~/src/ohr/games/wander/
+rm wandering-hamster.zip
+~/src/ohr/wip/ohrrpgce-custom -distrib zip wander.rpgdir
+if [ ! -f "wandering-hamster.zip" ] ; then
+  echo "Unable to export wandering-hamster.zip"
+  exit 1
+fi
+scp -p wandering-hamster.zip james_paige@motherhamster.org:HamsterRepublic.com/dl/
+rm setup-wandering-hamster.exe
+~/src/ohr/wip/ohrrpgce-custom -distrib win wander.rpgdir
+if [ ! -f "setup-wandering-hamster.exe" ] ; then
+  echo "Unable to export setup-wandering-hamster.exe"
+  exit 1
+fi
+scp -p setup-wandering-hamster.exe james_paige@motherhamster.org:HamsterRepublic.com/dl/wandering-hamster.exe
