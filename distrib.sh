06ebaf3b149018a1d8e8c27cab4ca1c00edc4dde
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-08-23 19:04:35 +0000

    Wander: improve my Wandering Hamster distrib script. Now exports and uploads all package formats.
    git-svn-id: https://rpg.hamsterrepublic.com/source/games/wander@9162 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- distrib.sh
+++ distrib.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
 
-cd ~/src/ohr/games/wander/
-rm wandering-hamster.zip
-~/src/ohr/wip/ohrrpgce-custom -distrib zip wander.rpgdir
-if [ ! -f "wandering-hamster.zip" ] ; then
-  echo "Unable to export wandering-hamster.zip"
-  exit 1
-fi
-scp -p wandering-hamster.zip james_paige@motherhamster.org:HamsterRepublic.com/dl/
-rm setup-wandering-hamster.exe
-~/src/ohr/wip/ohrrpgce-custom -distrib win wander.rpgdir
-if [ ! -f "setup-wandering-hamster.exe" ] ; then
-  echo "Unable to export setup-wandering-hamster.exe"
-  exit 1
-fi
-scp -p setup-wandering-hamster.exe james_paige@motherhamster.org:HamsterRepublic.com/dl/wandering-hamster.exe
+WANDERDIR=~/src/ohr/games/wander/
+OHRDIR=~/src/ohrrpgce/
+
+cd "${WANDERDIR}"
+
+FILES="\
+ wandering-hamster.zip \
+ wandering-hamster.exe \
+ wandering-hamster-mac.zip \
+ wandering-hamster-linux-x86.tar.gz \
+ wandering-hamster-linux-x86_64.tar.gz \
+ wandering-hamster_amd64.deb \
+ wandering-hamster_i386.deb \
+ "
+
+for FILE in ${FILES} ; do
+ rm -f "{$FILE}"
+done
+
+rm -f wandering-hamster_*.deb
+rm -f setup-wandering-hamster.exe
+
+"${OHRDIR}"/ohrrpgce-custom -distrib all wander.rpgdir
+mv setup-wandering-hamster.exe wandering-hamster.exe
+mv wandering-hamster_????.??.??_amd64.deb wandering-hamster_amd64.deb
+mv wandering-hamster_????.??.??_i386.deb wandering-hamster_i386.deb
+
+for FILE in ${FILES} ; do
+  if [ ! -f "${FILE}" ] ; then
+    echo "Unable to export ${FILE}"
+    exit 1
+  fi
+done
+
+for FILE in ${FILES} ; do
+  scp -p "${FILE}" james_paige@motherhamster.org:HamsterRepublic.com/dl/
+done
+
